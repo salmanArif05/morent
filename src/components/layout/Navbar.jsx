f16d95a9e8f4c0199acd29d0fb1d0fff7c58c6f4
salmanArif05 <salmanarif1010@gmail.com>
2023-10-07 04:02:59 +0500

    navbar

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -6,12 +6,18 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import Offcanvas from "react-bootstrap/Offcanvas";
+import { LuSearch } from "react-icons/lu";
+import { PiSlidersHorizontalThin } from "react-icons/pi";
+import { BsHeartFill } from "react-icons/bs";
+import { GoBellFill } from "react-icons/go";
+import { FaCog } from "react-icons/fa";
+import { HiMiniUser } from "react-icons/hi2";
 
 const headerNav = () => {
 	return (
 		<>
-			<Navbar expand={"lg"} className="bg-body-tertiary mb-3">
-				<Container fluid>
+			<Navbar expand={"lg"} className="headNav">
+				<Container>
 					<Navbar.Brand href="#">Morent</Navbar.Brand>
 					<Navbar.Toggle aria-controls={`offcanvasNavbar-expand-lg`} />
 					<Navbar.Offcanvas
@@ -22,17 +28,30 @@ const headerNav = () => {
 							<Offcanvas.Title id={`offcanvasNavbarLabel-expand-lg`}>Morent</Offcanvas.Title>
 						</Offcanvas.Header>
 						<Offcanvas.Body>
-							<Form className="d-flex position-relative">
-								<Form.Control type="search" placeholder="Search something here" className="me-2" aria-label="Search" />
-							</Form>
-							<Nav className="justify-content-end flex-grow-1 pe-3">
-								<Nav.Link href="#action1">Home</Nav.Link>
-								<Nav.Link href="#action2">Link</Nav.Link>
-								<NavDropdown title="Dropdown" id={`offcanvasNavbarDropdown-expand-lg`}>
-									<NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-									<NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
+							<div className="search">
+								<Form.Control type="search" placeholder="Search something here" />
+								<div className="search-icon">
+									<LuSearch />
+								</div>
+								<div className="slider-icon">
+									<PiSlidersHorizontalThin />
+								</div>
+							</div>
+							<Nav className="justify-content-end align-items-center flex-grow-1 pe-3 gap-3">
+								<Nav.Link href="#action1">
+									<BsHeartFill />
+								</Nav.Link>
+								<Nav.Link href="#action2" className="bell-notify">
+									<GoBellFill />
+								</Nav.Link>
+								<Nav.Link href="#action3">
+									<FaCog />
+								</Nav.Link>
+								<NavDropdown title={<HiMiniUser />} id={`offcanvasNavbarDropdown-expand-lg`}>
+									<NavDropdown.Item href="#action4">Profile</NavDropdown.Item>
+									<NavDropdown.Item href="#action5">Settings</NavDropdown.Item>
 									<NavDropdown.Divider />
-									<NavDropdown.Item href="#action5">Something else here</NavDropdown.Item>
+									<NavDropdown.Item href="#action6">Logout</NavDropdown.Item>
 								</NavDropdown>
 							</Nav>
 						</Offcanvas.Body>
